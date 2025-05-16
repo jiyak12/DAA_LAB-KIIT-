e82ba4896bd36e6236a1159e0aab0da6fd2f66ef
Jiya <jiyajha2020@gmail.com>
2025-05-17 01:15:35 +0530

    Create readme.cpp

---
+++ LAB-7/readme.cpp
@@ -0,0 +1,8 @@
+## Lab 7 
+1. `huff.cpp` Implement Huffman Coding for sequences:
+    - `BCCABBDDAECCBBAEDDCC`
+    - `122341322144344`
+2. `knap.cpp` Implement Fractional Knapsack with following strategies
+    - Non-increasing order of profit
+    - Non-decreasing order of weight
+    - Non-increasing order of profit/weight ratio
